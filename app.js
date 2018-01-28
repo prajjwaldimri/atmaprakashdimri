837811e1987d557b07e18f1d3afb9d3e5b8205b1
Prajjwal <prajjwaldimri@hotmail.com>
2018-01-28 15:33:42 +0530

    Replaced Semantic with bulma

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index', { pageTitle: 'Hey' });
+  res.render('index', { pageTitle: 'Home' });
+});
+
+app.get('/gallery', (req, res) => {
+  res.render('Gallery', { pageTitle: 'Gallery' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { pageTitle: 'About' });
+});
+
+app.get('/blog', (req, res) => {
+  res.render('blog', { pageTitle: 'Blog' });
+});
+
+app.get('/adminLogin', (req, res) => {
+  res.render('adminLogin', { pageTitle: 'Login for Administrator' });
 });
 
 app.listen('8080', () => {
