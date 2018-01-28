f9d714f19add536968a994056840e8cb0a47bf7f
Prajjwal <prajjwaldimri@hotmail.com>
2018-01-28 11:13:01 +0530

    Added Pug Templating Engine

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index', { pageTitle: 'Hey' });
 });
 
 app.listen('8080', () => {
