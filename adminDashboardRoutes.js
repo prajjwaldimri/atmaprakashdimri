9b3e774b12037b34b8b55e3a33c4e0f3b8ac6a8f
Prajjwal <prajjwaldimri@hotmail.com>
2018-02-10 11:08:07 +0530

    Update FileController

--- adminDashboardRoutes.js
+++ adminDashboardRoutes.js
@@ -1,72 +1,7 @@
-const path = require('path');
 const router = require('express').Router();
 const blogController = require('./controllers/blogController');
 const userController = require('./controllers/userController');
-const multer = require('multer');
-const mongoose = require('mongoose');
-let gridfsFile;
-let gridfsImage;
-let File;
-let Image;
-mongoose
-  .connect(
-    process.env.MONGODB_URL || 'mongodb://localhost/passport_local_mongoose'
-  )
-  .then(
-    () => {
-      gridfsFile = require('mongoose-gridfs')({
-        collection: 'files',
-        model: 'File',
-        mongooseConnection: mongoose.connection
-      });
-      File = gridfsFile.model;
-      gridfsImage = require('mongoose-gridfs')({
-        collection: 'images',
-        model: 'Image',
-        mongooseConnection: mongoose.connection
-      });
-      Image = gridfsImage.model;
-    },
-    err => {
-      console.log(err);
-    }
-  );
-const GridFsStorage = require('multer-gridfs-storage');
-const fileStorage = new GridFsStorage({
-  url: process.env.MONGODB_URL || 'mongodb://localhost/passport_local_mongoose',
-  file: (req, file) => {
-    return {
-      filename:
-        file.originalname + '-' + Date.now() + path.extname(file.originalname),
-      bucketName: 'files'
-    };
-  }
-});
-const imageStorage = new GridFsStorage({
-  url: process.env.MONGODB_URL || 'mongodb://localhost/passport_local_mongoose',
-  file: (req, file) => {
-    return {
-      filename:
-        file.originalname + '-' + Date.now() + path.extname(file.originalname),
-      bucketName: 'images'
-    };
-  }
-});
-
-const fileUpload = multer({
-  storage: fileStorage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb, /doc|docx|xls|xlsx|ppt|pdf|desktop/);
-  }
-}).single('uploadedFile');
-
-const imageUpload = multer({
-  storage: imageStorage,
-  limits: { fileSize: 50000000 },
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb, /jpeg|jpg|png|gif/);
-  }
-}).single('uploadedImage');
+const fileController = require('./controllers/fileController');
 
 // Request Authenticator
 router.use('/', (req, res, next) => {
@@ -77,79 +12,19 @@ router.use('/', (req, res, next) => {
   }
 });
 
-function checkFileType (file, cb, allowedFileTypes) {
-  // Check extension
-  const extName = allowedFileTypes.test(
-    path.extname(file.originalname).toLowerCase()
-  );
-  // Check MIME-TYPE
-  const mimeType = allowedFileTypes.test(file.mimetype);
-
-  if (extName && mimeType) {
-    return cb(null, true);
-  } else {
-    cb(new Error('File Type Not allowed!'));
-  }
-}
-
 router.get('/allBlogPosts', blogController.blog_list);
 
-router.get('/allUploadedFiles', (req, res) => {
-  File.find({}, (err, files) => {
-    if (err) {
-      req.flash('danger', err);
-      res.redirect('back');
-    }
-    res.render('dashboard/allUploadedFiles', {
-      pageTitle: 'List of uploaded files on server',
-      files: files
-    });
-  });
-});
+router.get('/allUploadedFiles', fileController.file_list);
 
-router.get('/allUploadedImages', (req, res) => {
-  Image.find({}, (err, images) => {
-    if (err) {
-      req.flash('danger', err);
-      res.redirect('back');
-    }
-    console.log(images);
-    res.render('dashboard/allUploadedImages', {
-      pageTitle: 'List of uploaded files on server',
-      images: images
-    });
-  });
-});
+router.get('/allUploadedImages', fileController.image_list);
 
-router.get('/downloadFile/:fileId', (req, res) => {
-  let fileStream = File.readById(req.params.fileId);
-  fileStream.pipe(res);
-});
+router.get('/downloadFile/:fileId', fileController.file_download);
 
-router.get('/deleteFile/:fileId', (req, res) => {
-  File.unlinkById(req.params.fileId, (err, unlinkedAttachment) => {
-    if (err) {
-      req.flash('danger', err);
-    }
-    req.flash('success', 'File successfully deleted');
-    res.redirect('back');
-  });
-});
+router.get('/deleteFile/:fileId', fileController.file_delete);
 
-router.get('/downloadImage/:fileId', (req, res) => {
-  let imageStream = Image.readById(req.params.fileId);
-  imageStream.pipe(res);
-});
+router.get('/downloadImage/:fileId', fileController.image_download);
 
-router.get('/deleteImage/:fileId', (req, res) => {
-  Image.unlinkById(req.params.fileId, (err, unlinkedAttachment) => {
-    if (err) {
-      req.flash('danger', err);
-    }
-    req.flash('success', 'Image successfully deleted');
-    res.redirect('back');
-  });
-});
+router.get('/deleteImage/:fileId', fileController.image_delete);
 
 router.get('/editBlogPost/:blogId', blogController.edit_blog_post_get);
 
@@ -173,43 +48,12 @@ router.get('/newFileUpload', (req, res) => {
   res.render('dashboard/newFileUpload', { pageTitle: 'Upload a file' });
 });
 
-router.post('/newFileUpload', (req, res) => {
-  fileUpload(req, res, err => {
-    if (err) {
-      req.flash('danger', 'Error Uploading File');
-      console.log(err);
-    } else {
-      req.flash('success', 'Successfully uploaded file');
-    }
-    res.redirect('/adminDashboard/allUploadedFiles');
-  });
-});
+router.post('/newFileUpload', fileController.file_upload);
 
 router.get('/newImageUpload', (req, res) => {
   res.render('dashboard/newImageUpload', { pageTitle: 'Upload an image' });
 });
 
-router.post('/newImageUpload', (req, res) => {
-  imageUpload(req, res, err => {
-    if (err) {
-      req.flash('danger', 'Error Uploading File');
-      console.log(err);
-    } else {
-      req.flash('success', 'Successfully uploaded image');
-    }
-    res.redirect('/adminDashboard/allUploadedImages');
-  });
-});
-
-router.post('/newImageUpload', (req, res) => {
-  imageUpload(req, res, err => {
-    if (err) {
-      req.flash('danger', 'Error uploading image');
-    } else {
-      req.flash('success', 'Successfully uploaded image');
-    }
-    res.redirect('/adminDashboard/allUploadedImages');
-  });
-});
+router.post('/newImageUpload', fileController.image_upload);
 
 module.exports = router;
---
+++ controllers/fileController.js
@@ -0,0 +1,161 @@
+let gridfsFile, File, gridfsImage, Image;
+const path = require('path');
+const multer = require('multer');
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    process.env.MONGODB_URL || 'mongodb://localhost/passport_local_mongoose'
+  )
+  .then(
+    () => {
+      gridfsFile = require('mongoose-gridfs')({
+        collection: 'files',
+        model: 'File',
+        mongooseConnection: mongoose.connection
+      });
+      File = gridfsFile.model;
+      gridfsImage = require('mongoose-gridfs')({
+        collection: 'images',
+        model: 'Image',
+        mongooseConnection: mongoose.connection
+      });
+      Image = gridfsImage.model;
+    },
+    err => {
+      console.log(err);
+    }
+  );
+
+const GridFsStorage = require('multer-gridfs-storage');
+const fileStorage = new GridFsStorage({
+  url: process.env.MONGODB_URL || 'mongodb://localhost/passport_local_mongoose',
+  file: (req, file) => {
+    return {
+      filename:
+        file.originalname + '-' + Date.now() + path.extname(file.originalname),
+      bucketName: 'files'
+    };
+  }
+});
+const imageStorage = new GridFsStorage({
+  url: process.env.MONGODB_URL || 'mongodb://localhost/passport_local_mongoose',
+  file: (req, file) => {
+    return {
+      filename:
+        file.originalname + '-' + Date.now() + path.extname(file.originalname),
+      bucketName: 'images'
+    };
+  }
+});
+
+const fileUpload = multer({
+  storage: fileStorage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb, /doc|docx|xls|xlsx|ppt|pdf|desktop/);
+  }
+}).single('uploadedFile');
+
+const imageUpload = multer({
+  storage: imageStorage,
+  limits: { fileSize: 50000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb, /jpeg|jpg|png|gif/);
+  }
+}).single('uploadedImage');
+
+function checkFileType (file, cb, allowedFileTypes) {
+  // Check extension
+  const extName = allowedFileTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  // Check MIME-TYPE
+  const mimeType = allowedFileTypes.test(file.mimetype);
+
+  if (extName && mimeType) {
+    return cb(null, true);
+  } else {
+    cb(new Error('File Type Not allowed!'));
+  }
+}
+
+exports.file_list = (req, res) => {
+  File.find({}, (err, files) => {
+    if (err) {
+      req.flash('danger', err);
+      res.redirect('back');
+    }
+    res.render('dashboard/allUploadedFiles', {
+      pageTitle: 'List of uploaded files on server',
+      files: files
+    });
+  });
+};
+
+exports.image_list = (req, res) => {
+  Image.find({}, (err, images) => {
+    if (err) {
+      req.flash('danger', err);
+      res.redirect('back');
+    }
+    console.log(images);
+    res.render('dashboard/allUploadedImages', {
+      pageTitle: 'List of uploaded files on server',
+      images: images
+    });
+  });
+};
+
+exports.file_download = (req, res) => {
+  let fileStream = File.readById(req.params.fileId);
+  fileStream.pipe(res);
+};
+
+exports.image_download = (req, res) => {
+  let imageStream = Image.readById(req.params.fileId);
+  imageStream.pipe(res);
+};
+
+exports.file_delete = (req, res) => {
+  File.unlinkById(req.params.fileId, (err, unlinkedAttachment) => {
+    if (err) {
+      req.flash('danger', err);
+    }
+    req.flash('success', 'File successfully deleted');
+    res.redirect('back');
+  });
+};
+
+exports.image_delete = (req, res) => {
+  Image.unlinkById(req.params.fileId, (err, unlinkedAttachment) => {
+    if (err) {
+      req.flash('danger', err);
+    }
+    req.flash('success', 'Image successfully deleted');
+    res.redirect('back');
+  });
+};
+
+exports.file_upload = (req, res) => {
+  fileUpload(req, res, err => {
+    if (err) {
+      req.flash('danger', 'Error Uploading File');
+      console.log(err);
+    } else {
+      req.flash('success', 'Successfully uploaded file');
+    }
+    res.redirect('/adminDashboard/allUploadedFiles');
+  });
+};
+
+exports.image_upload = (req, res) => {
+  imageUpload(req, res, err => {
+    if (err) {
+      req.flash('danger', 'Error Uploading File');
+      console.log(err);
+    } else {
+      req.flash('success', 'Successfully uploaded image');
+    }
+    res.redirect('/adminDashboard/allUploadedImages');
+  });
+};
