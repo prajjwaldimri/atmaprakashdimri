9abcf33b48fcaaf6caf05eb331871db2eea48159
Prajjwal <prajjwaldimri@hotmail.com>
2021-01-27 11:18:50 +0530

    Update downloads.vue

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -25,7 +25,7 @@
         td January 2021
         td
           a(
-            href="https://drive.google.com/file/d/16bAte793LHZPB0EBOPj8zTdKE8eNA0YK/view?usp=sharing",
+            href="https://drive.google.com/file/d/12_sZSF2fJx9EYdFilHvnNMN8itHmDkOc/view?usp=sharing",
             target="_blank"
           ) Click to download
           
