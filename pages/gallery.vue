4c3d185f073ec1415b852df19a1c25537949eaba
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2022-01-26 21:49:58 +0530

    Added images

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -26,6 +26,34 @@
       span.description In Rashtrapati Bhavan
 
     .gallery
+      img(src="~assets/president-award-1.jpg?webp", loading="lazy")
+      span.description In President's Calendar
+
+    .gallery
+      img(src="~assets/president-award-2.jpg?webp", loading="lazy")
+      span.description As a guest of President of India
+
+    .gallery
+      img(src="~assets/president-award-3.jpg?webp", loading="lazy")
+      span.description As a guest of President of India
+
+    .gallery
+      img(src="~assets/president-award-4.jpg?webp", loading="lazy")
+      span.description As a guest of President of India
+
+    .gallery
+      img(src="~assets/president-award-5.jpg?webp", loading="lazy")
+      span.description As a guest of President of India
+
+    .gallery
+      img(src="~assets/president-award-6.jpg?webp", loading="lazy")
+      span.description As a guest of President of India
+
+    .gallery
+      img(src="~assets/president-award-7.jpg?webp", loading="lazy")
+      span.description As a guest of President of India
+
+    .gallery
       img(src="~assets/youth-icon.jpg?webp", loading="lazy")
       span.description Receiving Youth Icon Award
 
@@ -38,16 +66,52 @@
       span.description Shailesh Matiyani Award
 
     .gallery
-      img(src="~assets/award-red-board.jpg?webp", loading="lazy")
-      span.description Recognition for excellent work
-
-    .gallery
       img(src="~assets/science-award.jpg?webp", loading="lazy")
       span.description Science Teacher Award by The National Academy of Sciences
 
     .gallery
+      img(src="~assets/best-teacher.jpg?webp", loading="lazy")
+      span.description Best teacher award
+
+    .gallery
+      img(src="~assets/election-award-1.jpg?webp", loading="lazy")
+      span.description Award for excellent work in Elections
+
+    .gallery
+      img(src="~assets/election-award-2.jpg?webp", loading="lazy")
+      span.description Award for excellent work in Elections
+
+    .gallery
+      img(src="~assets/inspire-award.jpg?webp", loading="lazy")
+      span.description INSPIRE Awards
+
+    .gallery
+      img(src="~assets/inspire-award-1.jpg?webp", loading="lazy")
+      span.description INSPIRE Awards
+
+    .gallery
+      img(src="~assets/education-minister.jpg?webp", loading="lazy")
+      span.description With Education Minister of Uttarakhand
+
+    .gallery
+      img(src="~assets/nehru-award.jpg?webp", loading="lazy")
+      span.description Award from Nehru Yuva Kendra for outstanding work in science
+
+    .gallery
+      img(src="~assets/education-minister-1.jpg?webp", loading="lazy")
+      span.description With Education Minister of Uttarakhand
+
+    .gallery
+      img(src="~assets/inspire-award-2.jpg?webp", loading="lazy")
+      span.description INSPIRE Awards
+
+    .gallery
       img(src="~assets/chamoli-times.jpg?webp", loading="lazy")
       span.description Award by Chamoli Times
+
+    .gallery
+      img(src="~assets/press-interview.jpg?webp", loading="lazy")
+      span.description Getting interviewed by the press
 
     .gallery
       img(src="~assets/president-group.jpg?webp", loading="lazy")
