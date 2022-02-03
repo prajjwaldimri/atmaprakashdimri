8da3dbf6b2b121654e0035e7eed5389de657cb5a
Prajjwal <prajjwaldimri@hotmail.com>
2022-02-03 23:31:52 +0530

    Update downloads.vue

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -25,7 +25,7 @@
         td February 2022
         td
           a(
-            href="https://drive.google.com/file/d/1t-TurhZf6l5PS9jFek2cXV-BD5UTK5dM/view?usp=sharing",
+            href="https://drive.google.com/file/d/1ahMplMyf5UHXG8z2CVIo4RDcg85PiLKC/view?usp=sharing",
             target="_blank"
           ) Click to download
 
